1c8288692d71fc021f105212760d1c71c32c9701
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-03-16 15:27:10 +0200

    Updates

---
+++ precache-manifest.9a63fcf677376e9026293fe639262698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "297b06b5b6843fee6463d13a7a159c50",
+    "url": "/chuck_jokes/index.html"
+  },
+  {
+    "revision": "b56282dc6f012757fd72",
+    "url": "/chuck_jokes/static/css/main.adbf41f6.chunk.css"
+  },
+  {
+    "revision": "7187e315d2e375c82f14",
+    "url": "/chuck_jokes/static/js/2.d24a4ab2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chuck_jokes/static/js/2.d24a4ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b56282dc6f012757fd72",
+    "url": "/chuck_jokes/static/js/main.89025be2.chunk.js"
+  },
+  {
+    "revision": "d3ca9cecfa3cfc939f39",
+    "url": "/chuck_jokes/static/js/runtime-main.1f8c2fe2.js"
+  },
+  {
+    "revision": "65689bfe79bdf67f84352f234ebbe830",
+    "url": "/chuck_jokes/static/media/6480919_preview.65689bfe.png"
+  }
+]);
